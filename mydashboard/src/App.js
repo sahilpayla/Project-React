075df0080e70bd9669f87aab845f28a83f4ecbc1
Shraddhanand <1shraddhanand@gmail.com>
2023-01-23 16:35:33 +0530

    project end

--- mydashboard/src/App.js
+++ mydashboard/src/App.js
@@ -14,13 +14,24 @@ export default class App extends Component {
   }
 
   fetchData = async () => {
-    let data = await fetch("https://api.coingecko.com/api/v3/coins/bitcoin");
-    let jsonData = await data.json()
-    this.setState({ Id: this.state.Id, Data: jsonData })
+    let data = await fetch('https://api.coingecko.com/api/v3/coins/' + this.state.Id)
+    let JsonData = await data.json()
+    this.setState({ Id: this.state.Id, Data: JsonData })
+  }
+
+  handleSubmit = async (event) => {
+    console.log(event.target.value)
+    await this.setState({ Id: event.target.value, Data: this.state.Data })
+    this.fetchData()
   }
 
   componentDidMount() {
-    this.fetchData();
+    this.fetchData()
+    this.interval = setInterval(() => this.fetchData(), 2000);
+
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   render() {
@@ -28,16 +39,16 @@ export default class App extends Component {
       <div>
         <Header handle_Submit={this.handleSubmit} />
 
-        <CardSection coinName={this.state.Data.name} 
+        <CardSection coinName={this.state.Data.name}
           currentPrice={this.state.Data.market_data ? this.state.Data.market_data.current_price["usd"] : ""}
           mCap24={this.state.Data.market_data ? this.state.Data.market_data.market_cap_change_percentage_24h : ""}
-          ath={this.state.Data.market_data ? this.state.Data.market_data.ath.usd : ""} 
+          ath={this.state.Data.market_data ? this.state.Data.market_data.ath.usd : ""}
           atl={this.state.Data.market_data ? this.state.Data.market_data.ath.usd : ""}
-          sentiment={this.state.Data.sentiment_votes_up_percentage} 
+          sentiment={this.state.Data.sentiment_votes_up_percentage}
           high24={this.state.Data.market_data ? this.state.Data.market_data.high_24h["usd"] : ""}
           low24={this.state.Data.market_data ? this.state.Data.market_data.low_24h["usd"] : ""} />
 
-        <ChartSection Id={this.state.Id} 
+        <ChartSection Id={this.state.Id}
           priceChange24={this.state.Data.market_data ? this.state.Data.market_data.price_change_24h_in_currency.usd : ""}
           MarketCap={this.state.Data.market_data ? this.state.Data.market_data.market_cap.usd : ""}
           TotVol={this.state.Data.market_data ? this.state.Data.market_data.total_volume.usd : ""}
