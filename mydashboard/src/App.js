e8279664ad56b0a954b6f1054b0baf95cfd6a486
Shraddhanand <1shraddhanand@gmail.com>
2023-01-23 15:20:55 +0530

    index file created with boottrap files

--- mydashboard/src/App.js
+++ mydashboard/src/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      Id: "bitcoin",
+      Data: {}
+    }
+  }
+
+  fetchData = async () => {
+    let data = await fetch("https://api.coingecko.com/api/v3/coins/bitcoin");
+    let jsonData = await data.json()
+    this.setState({ Id: this.state.Id, Data: jsonData })
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
   render() {
     return (
-      <div>App</div>
+      <div>
+        <h1>{this.state.Id}</h1>
+        <h2>{this.state.Data.name}</h2>
+        <h2>{this.state.Data.sentiment_votes_up_percentage}</h2>
+      </div>
     )
   }
 }
