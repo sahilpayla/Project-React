8858a7d2db0682885e06f0a7b773b9772a36ad42
Shraddhanand <1shraddhanand@gmail.com>
2023-01-25 17:18:28 +0530

    git add

--- header/src/components/navbar/Navbar.jsx
+++ header/src/components/navbar/Navbar.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import { React, useState } from 'react'
 import './Navbar.css';
-import { FaBars, FaTimes } from 'react-icons/fa'
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+
+
 const Navbar = () => {
+
+   const [click, setClick] = useState(false)
+   const handleClick = () => setClick(!click)
+
    return (
       <div className="navbar">
          <div className="logo">
             <img src="https://logopond.com/logos/cc6a2594468231d13bf7e305392c7dca.png" alt="" />
          </div>
-         <ul className='nav-menu'>
+         <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'><a href="/">Home</a></li>
             <li className='nav-item'><a href="/">About</a></li>
             <li className='nav-item'><a href="/">FAQ</a></li>
             <li className='nav-item'><a href="/">Contact</a></li>
          </ul>
-         <div className="hamburger">
-            <FaBars size={30} style={{color:'#f8f8f8'}}/>
+         <div className="hamburger" onClick={handleClick}>
+            {click ? (<FaTimes size={30} style={{ color: '#f8f8f8' }} />) : (<FaBars size={30} style={{ color: '#f8f8f8' }} />)}
          </div>
       </div>
    )
