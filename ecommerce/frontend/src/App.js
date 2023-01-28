8592535a0cb5420350268947df54b557053ea09f
Shraddhanand <1shraddhanand@gmail.com>
2023-01-28 17:42:08 +0530

    bootstrap added

--- ecommerce/frontend/src/App.js
+++ ecommerce/frontend/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      golu lolu
+        hello
     </div>
   );
 }
