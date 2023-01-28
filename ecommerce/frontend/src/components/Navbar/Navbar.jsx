012add3931ed6e716461337f2560a02714e3f2e6
Shraddhanand <1shraddhanand@gmail.com>
2023-01-28 19:41:45 +0530

    s

--- ecommerce/frontend/src/components/Navbar/Navbar.jsx
+++ ecommerce/frontend/src/components/Navbar/Navbar.jsx
@@ -11,9 +11,7 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
          <div className="container-fluid">
             <a className="navbar-brand" href="/">Game  Shopping</a>
-            {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-               <span className="navbar-toggler-icon"></span>
-            </button> */}
+
             <div className="collapse navbar-collapse" id="navbarNav">
                <ul className="navbar-nav ml-auto">
                   <li className="nav-item">
