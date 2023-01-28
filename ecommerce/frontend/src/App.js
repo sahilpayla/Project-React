9b006bd05a872a839759cb177a8eb7702220993a
Shraddhanand <1shraddhanand@gmail.com>
2023-01-28 19:40:07 +0530

    react router dom install

--- ecommerce/frontend/src/App.js
+++ ecommerce/frontend/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 
 
 function App() {
