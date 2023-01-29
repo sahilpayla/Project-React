1cc6955ad8b13d32e56d87660ed399c74bc8c850
Shraddhanand <1shraddhanand@gmail.com>
2023-01-29 13:27:42 +0530

    add photos

--- ecommerce/frontend/src/components/Game.jsx
+++ ecommerce/frontend/src/components/Game.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
-import data from '../consoledata'
+import { React, useState } from 'react';
+import data from '../consoledata';
+import './Game.css'
 
 const Game = ({ game }) => {
+
+
+   const [quantity, setQuantity] = useState(1)
+   const [varient, setVarient] = useState('small')
+
    return (
       <>
+         {/* name and image convention */}
          <h1>{game.name}</h1>
-         <img src={game.image} className="img-fluid" alt="" 
-         style={{ height: "450px", width: "350px" }} />
+         <img src={game.image} className="img-fluid" alt=""
+            style={{ height: "450px", width: "350px" }} />
+
+
+         {/* options size && Quantity */}
+         <div className="flex-container">
+            <div className="w-100">
+               <p>
+                  Varients
+               </p>
+               <select className='form-control' value={varient} onChange={(e) => { setVarient(e.target.value) }}>
+                  {game.varients.map(varient => {
+                     return <option value={varient}>{varient} </option>
+                  })}
+               </select>
+            </div>
+            <div className="w-100">
+               <p>
+                  Quantity
+               </p>
+               <select className='form-control' value={quantity} onChange={(e) => { setQuantity(e.target.value) }}>
+                  {[...Array(10).keys()].map((x, i) => {
+                     return <option value={i + 1}>
+                        {i + 1}
+                     </option>
+                  })}
+               </select>
+            </div>
+         </div>
+
+         <div className="flex-container">
+            <div className='ml-0'>
+               <h1>Price : {game.prices[0][varient] * quantity}</h1>
+            </div>
+            <div className=' '>
+               <button className="btn">Add To Cart</button>
+            </div>
+         </div>
       </>
    )
 }
--- ecommerce/frontend/src/consoledata.js
+++ ecommerce/frontend/src/consoledata.js
@@ -25,12 +25,12 @@ const consoleData = [
          }
       ],
       category: "nonveg",
-      image: "https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/640/IV.jpg",
+      image: "https://cdn-products.eneba.com/resized-products/3JqIyCFQIl87ioaZC4oSqLx878AXHQIoKSvASELch4E_350x200_1x-0.jpeg",
       description: "GTA IV Console Game Disc"
    },
 
    {
-      name: "GTA V",
+      name: "Watch Dogs",
       varients: ["small", "medium", "large"],
       prices: [
          {
@@ -40,7 +40,7 @@ const consoleData = [
          }
       ],
       category: "nonveg",
-      image: "https://m.media-amazon.com/images/I/81AhbROHiHL._SL1500_.jpg",
+      image: "https://zozila.com/wp-content/uploads/2021/01/F647F600-722B-4506-B09C-CFB34DD6E74A1.jpeg",
       description: "GTA V Console Game Disc"
    },
 
@@ -55,7 +55,7 @@ const consoleData = [
          }
       ],
       category: "nonveg",
-      image: "https://m.media-amazon.com/images/I/81AhbROHiHL._SL1500_.jpg",
+      image: "https://sgp1.digitaloceanspaces.com/relay-bl-in-records/GameNation/EA_29",
       description: "GTA IV Console Game Disc"
    }, 
    
@@ -70,6 +70,35 @@ const consoleData = [
          }
       ],
       category: "nonveg",
+      image: "https://m.media-amazon.com/images/I/61J495E7pCL._SL1000_.jpg",
+      description: "GTA V Console Game Disc"
+   },
+
+   {
+      name: "GTA IV",
+      varients: ["small", "medium", "large"],
+      prices: [
+         {
+            small: 200,
+            medium: 350,
+            large: 450
+         }
+      ],
+      category: "nonveg",
+      image: "https://i0.wp.com/electronicsstores01.com/wp-content/uploads/2021/04/81fkPmPf3XL._SL1500_.jpg?fit=1195%2C1500&ssl=1",
+      description: "GTA IV Console Game Disc"
+   },
+   {
+      name: "GTA V",
+      varients: ["small", "medium", "large"],
+      prices: [
+         {
+            small: 200,
+            medium: 350,
+            large: 450
+         }
+      ],
+      category: "nonveg",
       image: "https://m.media-amazon.com/images/I/81AhbROHiHL._SL1500_.jpg",
       description: "GTA V Console Game Disc"
    },
@@ -85,7 +114,67 @@ const consoleData = [
          }
       ],
       category: "nonveg",
-      image: "https://m.media-amazon.com/images/I/81AhbROHiHL._SL1500_.jpg",
+      image: "https://images.jdmagicbox.com/quickquotes/images_main/bethesda-dishonored-2-gaming-cd-ps4-2116056044-j2pqvmzh.jpg",
+      description: "GTA IV Console Game Disc"
+   },
+
+   {
+      name: "Watch Dogs",
+      varients: ["small", "medium", "large"],
+      prices: [
+         {
+            small: 200,
+            medium: 350,
+            large: 450
+         }
+      ],
+      category: "nonveg",
+      image: "https://zozila.com/wp-content/uploads/2021/01/F647F600-722B-4506-B09C-CFB34DD6E74A1.jpeg",
+      description: "GTA V Console Game Disc"
+   },
+
+   {
+      name: "GTA IV",
+      varients: ["small", "medium", "large"],
+      prices: [
+         {
+            small: 200,
+            medium: 350,
+            large: 450
+         }
+      ],
+      category: "nonveg",
+      image: "https://sgp1.digitaloceanspaces.com/relay-bl-in-records/GameNation/EA_29",
+      description: "GTA IV Console Game Disc"
+   }, 
+   
+   {
+      name: "GTA V",
+      varients: ["small", "medium", "large"],
+      prices: [
+         {
+            small: 200,
+            medium: 350,
+            large: 450
+         }
+      ],
+      category: "nonveg",
+      image: "https://m.media-amazon.com/images/I/61J495E7pCL._SL1000_.jpg",
+      description: "GTA V Console Game Disc"
+   },
+
+   {
+      name: "GTA IV",
+      varients: ["small", "medium", "large"],
+      prices: [
+         {
+            small: 200,
+            medium: 350,
+            large: 450
+         }
+      ],
+      category: "nonveg",
+      image: "https://sgp1.digitaloceanspaces.com/relay-bl-in-records/GameNation/EA_163",
       description: "GTA IV Console Game Disc"
    },
 ]
