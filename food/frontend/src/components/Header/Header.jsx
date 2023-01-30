630d97cc9edf2dfed45b3ee81c1ecd67e4d8a16f
Shraddhanand <1shraddhanand@gmail.com>
2023-01-30 14:26:24 +0530

    set navbar

--- food/frontend/src/components/Header/Header.jsx
+++ food/frontend/src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import './Header.css'
 
 
 const Header = () => {
    return (
 
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-light">
          <div className="container-fluid">
-            <Link className="navbar-brand" to="/">Food Website</Link>
+            <Link className="navbar-brand-image" to="/">
+               <img src="https://images.ctfassets.net/9mt4r6ro1ay9/4sZ8eIRj8syEOCoqsEkCcy/7ad6202bea061f90fc4ac27db407a7b1/apple_logo.png" alt="" />
+            </Link>
+            <Link className="navbar-brand" to="/">Apple Store</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                <span className="navbar-toggler-icon"></span>
             </button>
