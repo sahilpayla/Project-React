5bc4cf7f1bdba7b7b3f604b27f0844a835094360
Shraddhanand <1shraddhanand@gmail.com>
2023-01-30 11:50:37 +0530

    add footer

--- food/frontend/src/App.js
+++ food/frontend/src/App.js
@@ -1,10 +1,11 @@
-// import logo from './logo.svg';
+
 import './App.css';
+import Home from './screens/Home';
 
 function App() {
   return (
     <div className="App">
-    hello
+      <Home />
     </div>
   );
 }
