109fe093d6eb345beb2cd6105b19919ee96a6d3b
Shraddhanand <1shraddhanand@gmail.com>
2023-02-02 11:17:36 +0530

    still the project is working with no error npm start also

--- food/frontend/src/components/Card/Card.jsx
+++ food/frontend/src/components/Card/Card.jsx
@@ -3,56 +3,58 @@ import { useNavigate } from 'react-router-dom'
 import { useDispatchCart, useCart } from '../contextReducer'
 
 export default function Card(props) {
-   let data = useCart();
-
+   
    let navigate = useNavigate()
    const [qty, setQty] = useState(1)
    const [size, setSize] = useState("")
    const priceRef = useRef();
+   let data = useCart();
 
    let options = props.options;
    let pricesOptions = Object.keys(options);
-   let foodItem = props.item; 
+   let foodItem = props.item;
    let dispatch = useDispatchCart();
+
+
+   const handleAddToCart = async () => {
+      // let food = []
+      // for (const item of data) {
+      //    if (item.id === foodItem._id) {
+      //       food = item;
+
+      //       break;
+      //    }
+      // }
+
+      // console.log(food)
+      // console.log(new Date())
+
+      // if (food !== []) {
+      //    if (food.size === size) {
+      //       await dispatch({ type: "UPDATE", id: props.foodItem._id, price: finalPrice, qty: qty })
+      //       return
+      //    }
+      //    else if (food.size !== size) {
+      //       await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size, img: props.ImgSrc })
+      //       console.log("Size different so simply ADD one more to the list")
+      //       return
+      //    }
+      //    return
+      // }
+
+      await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size })
+      await console.log(data)
+
+   }
+
+   let finalPrice = qty * parseInt(options[size]);   //This is where Price is changing
 
    useEffect(() => {
       setSize(priceRef.current.value)
    }, [])
 
 
-   const handleAddToCart = async () => {
-      let food = []
-      for (const item of data) {
-         if (item.id === foodItem._id) {
-            food = item;
 
-            break;
-         }
-      }
-      console.log(food)
-      console.log(new Date())
-      if (food !== []) {
-         if (food.size === size) {
-            await dispatch({ type: "UPDATE", id: foodItem._id, price: finalPrice, qty: qty })
-            return
-         }
-         else if (food.size !== size) {
-            await dispatch({ type: "ADD", id: foodItem._id, name: foodItem.name, price: finalPrice, qty: qty, size: size, img: props.ImgSrc })
-            console.log("Size different so simply ADD one more to the list")
-            return
-         }
-         return
-      }
-
-      await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size })
-
-   }
-
-
-
-   let finalPrice = qty * parseInt(options[size]);   //This is where Price is changing
-   // totval += finalPrice;
-   // console.log(totval)
 
    return (
       <div >
