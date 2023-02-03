0f739156594f9dd466c2b2012335c588aa2c31f3
Shraddhanand <1shraddhanand@gmail.com>
2023-02-03 21:31:26 +0530

    start navbar

--- cafe_website/src/components/Navbar/Navbar.jsx
+++ cafe_website/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/gi';
+import images from '../../constants/images'
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
+  <nav className='app__navbar'>
     Navbar
-  </div>
+  </nav>
 );
 
 export default Navbar;
