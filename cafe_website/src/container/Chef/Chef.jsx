4d22e9a45123b29964b5d9c36a099315b0a6b1d9
Shraddhanand <1shraddhanand@gmail.com>
2023-02-04 15:31:07 +0530

    complete chef

--- cafe_website/src/container/Chef/Chef.jsx
+++ cafe_website/src/container/Chef/Chef.jsx
@@ -10,8 +10,26 @@ const Chef = () => (
     </div>
 
     <div className="app__wrapper_info">
-      <SubHeading title="Chef's Words"/>
+      <SubHeading title="Chef's Words" />
       <h1 className='headtext__cormorant'>What We Believe In</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ad accusamus pariatur 
+          </p>
+        </div>
+        <p className="p__opensans">
+         minima aspernatur minus excepturi. Neque odit fugit, unde sequi illo omnis, officiis quaerat libero doloribus impedit officia magni!
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p >Kevin Lua</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="" />
+      </div>
     </div>
   </div>
 );
