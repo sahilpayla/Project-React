2e8ee8853adbf1fc15629094ba678cf4cffe0713
Shraddhanand <1shraddhanand@gmail.com>
2023-02-09 21:21:46 +0530

    footer-complete

---
+++ apple/src/components/Footer/Footer.jsx
@@ -0,0 +1,147 @@
+import React from 'react';
+import './Footer.css';
+import { a } from 'react-router-dom';
+
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer-container">
+        <>
+          <p>
+            *Data plan is required. 5G is available in selected markets and through selected carriers. Speeds vary based on site conditions and carrier. For details on 5G support, contact your carrier and see apple.com/in/iphone/cellular.
+          </p>
+          <p>
+            ‡Instant savings, otherwise referred to as Instant Cashback on the Apple Store Online, is available with the purchase of an eligible product for qualifying HDFC Bank Credit Cards & HDFC Bank Credit Card EMI only. Minimum transaction value of ₹10001 applies. Click here to see instant savings amounts and eligible devices. Instant savings is available for up to two orders per rolling 90-day period with an eligible card. Card eligibility is subject to terms and conditions between you and your card-issuing bank. Total transaction value is calculated after any trade-in credit or eligible discount applied. Any subsequent order adjustment(s) or cancellation(s) may result in instant savings being recalculated, and any refund may be adjusted to account for instant savings clawback; this may result in no refund being made to you. Offer may be revised or withdrawn at any time without any prior notice. Terms and conditions apply. Offer cannot be combined with Apple Store for Education or Corporate Employee Purchase Plan pricing. Multiple separate orders cannot be combined for instant savings.
+          </p>
+          <p>
+            Get extra trade-in credit, otherwise referred to as additional exchange savings or exchange bonus, when you trade in an eligible iPhone and purchase a new Apple device until 5 March 2023. Extra trade-in credit of ₹5000 is available while trading in iPhone 12, iPhone 11, iPhone XR or iPhone X and ₹4500 on iPhone 7.
+          </p>
+          <p>
+            ‡‡No Cost EMI is available with the purchase of an eligible product made using qualifying credit or debit cards on 3- or 6-month tenures from most leading banks. Minimum order spend applies as per your credit or debit card’s issuing bank threshold. Offer cannot be combined with Apple Store for Education or Corporate Employee Purchase Plan pricing.
+            Credit or debit card eligibility is subject to terms and conditions between you and your credit or debit card–issuing bank. Offer may be revised or withdrawn at any time without any prior notice. Terms & conditions apply.
+          </p>
+          <p>
+            Representative example: Based off a 6-month tenure. iPhone 14 at ₹79900 total cost includes 14% p.a. and No Cost EMI savings of ₹3164, paid over 6 months as six monthly payments of ₹13317.
+          </p>
+        </>
+
+        <hr className='footer-hr' />
+
+        <div className="footer-bottom">
+          <div className="footer-row1">
+            <div className="footer-heading">Shop and Learn</div>
+            <div className="footer-points">
+              <a className='footer-points-micro' href="" to="/">Store</a>
+              <a className='footer-points-micro' href="" to="/">Mac</a>
+              <a className='footer-points-micro' href="" to="/">iPad</a>
+              <a className='footer-points-micro' href="" to="/">iPhone</a>
+              <a className='footer-points-micro' href="" to="/">Watch</a>
+              <a className='footer-points-micro' href="" to="/">Airpods</a>
+              <a className='footer-points-micro' href="" to="/">TV & Home</a>
+              <a className='footer-points-micro' href="" to="/">AirTag</a>
+              <a className='footer-points-micro' href="" to="/">Accessories</a>
+              <a className='footer-points-micro' href="" to="/">Gift Cards</a>
+            </div>
+            <div className="footer-heading">Apple Wallet</div>
+            <div className="footer-points">
+              <a className='footer-points-micro' href="" to="/">Wallet</a>
+            </div>
+          </div>
+          <div className="footer-row1">
+            <div className="footer-heading">Account</div>
+            <div className="footer-points">
+              <a className='footer-points-micro' href="" to="/">Manage Your Apple ID</a>
+              <a className='footer-points-micro' href="" to="/">Apple Store Account</a>
+              <a className='footer-points-micro' href="" to="/">iCloud.com</a>
+
+              <div className="footer-heading">Entertaiment</div>
+              <div className="footer-points">
+                <a className='footer-points-micro' href="" to="/">Apple One</a>
+                <a className='footer-points-micro' href="" to="/">Apple TV+</a>
+                <a className='footer-points-micro' href="" to="/">Apple Music</a>
+                <a className='footer-points-micro' href="" to="/">Apple Arcade</a>
+                <a className='footer-points-micro' href="" to="/">Apple Podcasts</a>
+                <a className='footer-points-micro' href="" to="/">Apple Books</a>
+                <a className='footer-points-micro' href="" to="/">Apple Store</a>
+              </div>
+            </div>
+          </div>
+          <div className="footer-row1">
+            <div className="footer-heading">Apple Store</div>
+            <div className="footer-points">
+              <a className='footer-points-micro' href="" to="/">Ways to Buy</a>
+              <a className='footer-points-micro' href="" to="/">Apple Trade In</a>
+              <a className='footer-points-micro' href="" to="/">Recycling Programme</a>
+              <a className='footer-points-micro' href="" to="/">Order Status</a>
+              <a className='footer-points-micro' href="" to="/">Shopping Help</a>
+            </div>
+          </div>
+          <div className="footer-row1">
+            <div className="footer-heading">For Business</div>
+            <div className="footer-points">
+              <a className='footer-points-micro' href="" to="/">Apple Business</a>
+            </div>
+            <div className="footer-heading">For Education</div>
+            <div className="footer-points">
+              <a className='footer-points-micro' href="" to="/">Apple and Education</a>
+              <a className='footer-points-micro' href="" to="/">Shop for Education</a>
+              <a className='footer-points-micro' href="" to="/">Shop for University</a>
+            </div>
+            <div className="footer-heading">For Healthcare</div>
+            <div className="footer-points">
+              <a className='footer-points-micro' href="" to="/">Apple in Healthcare</a>
+              <a className='footer-points-micro' href="" to="/">Health on Apple Watch</a>
+            </div>
+          </div>
+          <div className="footer-row1">
+            <div className="footer-heading">Apple Values</div>
+            <div className="footer-points">
+              <a className='footer-points-micro' href="" to="/">Accessibility</a>
+              <a className='footer-points-micro' href="" to="/">Education</a>
+              <a className='footer-points-micro' href="" to="/">Environment</a>
+              <a className='footer-points-micro' href="" to="/">Privacy</a>
+              <a className='footer-points-micro' href="" to="/">Supplier Responsibility</a>
+            </div>
+
+            <div className="footer-heading">About Apple </div>
+            <div className="footer-points">
+              <a className='footer-points-micro' href="" to="/">Newsroom</a>
+              <a className='footer-points-micro' href="" to="/">Apple Leadership</a>
+              <a className='footer-points-micro' href="" to="/">Career Opportunities</a>
+              <a className='footer-points-micro' href="" to="/">Investors</a>
+              <a className='footer-points-micro' href="" to="/">Ethics & Compliance</a>
+              <a className='footer-points-micro' href="" to="/">Events</a>
+              <a className='footer-points-micro' href="" to="/">Contact Apple</a>
+            </div>
+          </div>
+        </div>
+        <p>More ways to shop: <a style={{ color: "#06c" }}>Find a retailer</a> near you. Or call 000800 040 1966.</p>
+
+        <hr className='footer-hr' />
+
+        <div className="footer-last">
+          <div className="footer-flex">
+            <div className="footer-last-1">
+              Copyright © 2023 Apple Inc. All rights reserved.
+            </div>
+
+            <div className="footer-last-2">
+              <div className='footer-privacy'>Privacy Policy </div>
+              <span>Terms of Use </span>
+              <span>Sales Policy  </span>
+              <span>Legal  </span>
+              <span>Site Map </span>
+            </div>
+          </div>
+
+          <div className="footer-last-3">
+            India
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Footer
---
+++ apple/src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from '../components/Footer/Footer'
+
+const Homepage = () => {
+  return (
+   <>
+      <Footer />
+   </>
+  )
+}
+
+export default Homepage
