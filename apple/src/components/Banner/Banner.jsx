6098e74a5220bfbffe7fb283bb526ee4d88f8a1d
Shraddhanand <1shraddhanand@gmail.com>
2023-02-10 11:18:56 +0530

    header start

---
+++ apple/src/components/Banner/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Banner.css'
+
+const Banner = () => {
+   return (
+      <>
+         <div className="header-banner">
+            <p>
+               Get up to ₹10000 instant savings on selected products with HDFC Bank credit cards.‡ Plus No Cost EMI from most leading banks.‡‡
+               <a href="/" className='shopnow'> Shop now › </a>
+            </p>
+         </div>
+      </>
+   )
+}
+
+export default Banner
---
+++ apple/src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Header.css';
+
+
+const Header = () => {
+   return (
+      <>
+         <div className="header">
+            <div className="header-points">Logo</div>
+            <div className="header-points">Store</div>
+            <div className="header-points">Mac</div>
+            <div className="header-points">iPad</div>
+            <div className="header-points">iPhone</div>
+            <div className="header-points">Watch</div>
+            <div className="header-points">AirPods</div>
+            <div className="header-points">TV & Home</div>
+            <div className="header-points">Entertaiment</div>
+            <div className="header-points">Accessories</div>
+            <div className="header-points">Support</div>
+            <div className="header-points">Search</div>
+            <div className="header-points">Bag</div>
+         </div>
+         
+      </>
+   )
+}
+
+export default Header
