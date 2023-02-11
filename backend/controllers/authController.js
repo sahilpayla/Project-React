568733e46a6d9db196c5091f8fd03b234e16eb1a
Shraddhanand <1shraddhanand@gmail.com>
2023-02-11 15:18:56 +0530

    models are correct

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -26,5 +26,24 @@ authController.post('/register', async(req, res) => {
 })
 
 // login
+authController.post('/login', async(req, res) => {
+   try {
+      const isExisting = await User.findOne({ email: req.body.email })
+      if (isExisting) {
+         throw new Error("Already such an account with this email. Try a new one!")
+      }
+
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+      const newUser = await User.create({ ...req.body, password: hashedPassword })
+      const { password, ...others } = newUser._doc
+      const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, secret, { expiresIn: '5h' })
+
+      return res.status(201).json({ others, token })
+
+   } catch (error) {
+      return res.status(500).json(error.message)
+   }
+})
 
 module.exports = authController
