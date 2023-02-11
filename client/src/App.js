c3b7c5ec347621625eb07ee1c6ff9111fb78e650
Shraddhanand <1shraddhanand@gmail.com>
2023-02-11 20:38:56 +0530

    s

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home.jsx';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import Fooddetails from './components/Fooddetails/Fooddetails';
+import Foodcatalog from './components/Foodcatalog/Foodcatalog';
+import Cart from './components/Cart/Cart';
+import Checkout from './components/Checkout/Checkout';
+import Create from './components/Create/Create';
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Footer/>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/create' element={<Create />} />
+        <Route path='/food/:id' element={<Fooddetails />} />
+        <Route path='/foods/:id' element={<Foodcatalog />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/checkout' element={<Checkout />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
