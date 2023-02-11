32849a106525c9b875e9d18ea4ea6f2c5ca203ac
Shraddhanand <1shraddhanand@gmail.com>
2023-02-11 21:53:13 +0530

    navbar complete backend complete

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import {AiOutlineShoppingCart, AiOutlineUser} from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      <div className="navbar-container">
+        <div className="navbar-wrapper">
+          <div className="navbar-left">
+            <Link to='/' className="navbar-title">Martin Store</Link>
+          </div>
+          <div className="navbar-center">
+            <div className="navbar-list">
+              <div className="navbar-list-item"><a href="/">Home</a></div>
+              <div className="navbar-list-item"><a href="/">Contact</a></div>
+              <div className="navbar-list-item"><a href="/">Foods</a></div>
+              <div className="navbar-list-item"><a href="/">FAQs</a></div>
+              <div className="navbar-list-item"><Link to="/create">Create</Link></div>
+            </div>
+          </div>
+          <div className="navbar-right">
+              <AiOutlineUser className='navbar-userIcon' />
+              <Link to='/cart' className='navbar-cartContainer'>
+                <AiOutlineShoppingCart className='navbar-cartIcon' />
+                <div className="navbar-cartQuantity">0</div>
+              </Link>
+              <button className="navbar-logout">Logout</button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
