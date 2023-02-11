d2af7c2be29e6a03f78e445af88af77446d4b9b5
Shraddhanand <1shraddhanand@gmail.com>
2023-02-11 14:29:43 +0530

    models are correct

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,30 @@
 const authController = require('express').Router();
-const user = require('../models/user.js');
+const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secret = "jwtSecret"
 
+//register 
+authController.post('/register', async(req, res) => {
+   try {
+      const isExisting = await User.findOne({ email: req.body.email })
+      if (isExisting) {
+         throw new Error("Already such an account with this email. Try a new one!")
+      }
+
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+      const newUser = await User.create({ ...req.body, password: hashedPassword })
+      const { password, ...others } = newUser._doc
+      const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, secret, { expiresIn: '5h' })
+
+      return res.status(201).json({ others, token })
+
+   } catch (error) {
+      return res.status(500).json(error.message)
+   }
+})
+
+// login
+
+module.exports = authController
--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,28 @@
-const URI = 'mongodb+srv://user:user@martin-store.sjyk71w.mongodb.net/?retryWrites=true&w=majority';
 const express = require('express');
 const app = express();
 const port = 8000;
 const mongoose = require('mongoose');
 const cors = require('cors');
+const authController = require('./controllers/authController.js');
 
 
-app.use(cors());
-// app.use('/', (req, res) => {
-//    console.log('hellow world')
-//    res.send('hello')
-// })
-
-
+// database
 mongoose.set('strictQuery', false);
+let URI = 'mongodb+srv://user:user@martin-store.sjyk71w.mongodb.net/store?retryWrites=true&w=majority';
 mongoose.connect(URI, () => {
    console.log('Connected To The Database')
 })
 
 
+
+// routes and middlewares
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use('/auth', authController)
+
+
+// server
 app.listen(port, () => {
    console.log(`Server is running at ${port}`)
 })
