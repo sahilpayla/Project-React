5c9300d01a87548a55084f2a68c74af16a21c2fe
Shraddhanand <1shraddhanand@gmail.com>
2023-02-11 18:49:56 +0530

    front end start

---
+++ client/src/components/Foodcatalog/Foodcatalog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Foodcatalog = () => {
+  return (
+    <div>Foodcatalog</div>
+  )
+}
+
+export default Foodcatalog
---
+++ client/src/components/Fooddetails/Fooddetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fooddetails = () => {
+  return (
+    <div>Fooddetails</div>
+  )
+}
+
+export default Fooddetails
