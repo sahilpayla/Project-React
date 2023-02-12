21fe6a4d2b66cc4ae8563f53006fc2fffa03f25b
Shraddhanand <1shraddhanand@gmail.com>
2023-02-12 16:48:05 +0530

    start home section

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,7 +4,31 @@ import Hero from '../Hero/Hero'
 const Home = () => {
   return (
     <>
-      <Hero/>
+      <div className="container">
+        <div className="wrapper">
+          <Hero />
+          <div className="delivery">
+            <div className="titles">
+              <span className="deliverySubtitle">Delivery</span>
+              <h2 className="deliveryTitle">Always on time for you</h2>
+            </div>
+            <div className="deliveryInfos">
+              <div className="deliveryInfo">
+                <img src={illustration1} alt="" className='firstImg' />
+                <h3>Our delivery guy is always on time</h3>
+              </div>
+              <div className="deliveryInfo">
+                <img src={illustration2} alt="" className='secondImg' />
+                <h3>He works very hard</h3>
+              </div>
+              <div className="deliveryInfo">
+                <img src={illustration3} alt="" className='thirdImg' />
+                <h3>He is friendly and social</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
