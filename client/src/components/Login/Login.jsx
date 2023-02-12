0058a7a711807e86a43176b70e24c3e6646ca82b
Shraddhanand <1shraddhanand@gmail.com>
2023-02-12 21:54:19 +0530

    login start

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import img from '../../assets/womaneating2.jpg'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
+// import { useDispatch } from 'react-redux';
+import './Login.css';
+
+
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  // const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+
+    try {
+        const res = await fetch(`http://localhost:8000/auth/login`, {
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+        method: "POST",
+        body: JSON.string
+      }) 
+    }
+    catch (error) {
+      setError(true)
+      setTimeout(() => {
+        setError(false)
+      }, 3000);
+    }
+  }
+
   return (
-    <div>Login</div>
+    <>
+      <div className="login-container">
+        <div className="login-wrapper">
+          <div className="login-left">
+            <img src={img} alt="" className='login-leftImg' />
+          </div>
+          <div className="login-right">
+            <h2 className="login-title">Login</h2>
+            <form onSubmit={handleLogin} className='login-form'>
+              <input type="email" placeholder='Type email' onChange={(e) => setEmail(e.target.value)} />
+              <input type="password" placeholder='Type password' onChange={(e) => setPassword(e.target.value)} />
+              <button className="login-submitBtn">Login</button>
+              <p>Don't have an account <Link to='/signup'>Sign up</Link> </p>
+            </form>
+
+
+            {
+              error && <div className="login-errorMessage">
+                Wrong Password or Email !
+              </div>
+            }
+
+
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
